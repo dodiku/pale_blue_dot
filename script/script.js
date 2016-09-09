d5c897df09a41b8094c4af1e62f794a3a394b2a5
dodiku <ayalon.dror@gmail.com>
2016-09-09 17:37:16 -0400

    trying to center canvas #2

--- script/script.js
+++ script/script.js
@@ -1,21 +1,39 @@
-var height, width, canvasHeight, canvasWidth;
-var timeInterval = 3000;
+var canvasHeight, canvasWidth, cnv;
 
-var myCanvas = createCanvas(winWidth, winHeight);
+var timeInterval = 3000;
+var h = 40;
+var w = 40;
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
 
 function setup() {
   canvasWidth = 400;
   canvasHeight = 400;
-  var myCanvas = createCanvas(canvasWidth, canvasHeight);
-  myCanvas.parent('canvas');
-  height = 40;
-  width = 40;
+  cnv = createCanvas(canvasWidth, canvasHeight);
+  // centerCanvas();
+  //
+  //
+  // // var myCanvas = createCanvas(canvasWidth, canvasHeight);
+  // // myCanvas.parent('canvas');
+  // var x = (windowWidth - width) / 2;
+  // var y = (windowHeight - height) / 2;
+  // myCanvas.position(x, y);
+  smooth();
+	// ellipse(200, 200, w, h);
+}
+
+function windowResized() {
+  centerCanvas();
 }
 
 function draw() {
   background(220);
   smooth();
-	ellipse(200, 200, width, height);
+	ellipse(200, 200, w, h);
 }
 
 function mousePressed(){
@@ -24,22 +42,22 @@ function mousePressed(){
   console.log("reseting timeInterval to 3000");
   timeInterval = 3000;
 
-  if (width >= canvasWidth){
+  if (w >= canvasWidth){
     //reseting the circle
-    width = 40;
-    height = 40;
+    w = 40;
+    h = 40;
   }
   else{
     console.log("else");
-    width = width + 20;
-    height = height + 20;
+    w = w + 20;
+    h = h + 20;
     setTimeout(reduceSize, timeInterval);
   }
 }
 
 function reduceSize () {
   console.log("reducing size...");
-    width = width - 20;
-    height = height - 20;
+    w = w - 20;
+    h = h - 20;
 
 }
