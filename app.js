d8eca803fefe4d7eee6c73da3604763f35839448
dodiku <ayalon.dror@gmail.com>
2016-09-13 14:19:45 -0400

    debug version

--- app.js
+++ app.js
@@ -19,23 +19,23 @@ var port = process.env.PORT || 3000;
 // CONFIGURATIONS
 // *************************
 
-// debug variables
-// var h = 20;
-// var w = 20;
-// var increment = 8;
-// var limit = 400;
-// var timeInterval = 3000;
-// var restartInterval = 300000;
-// var final = 0;
-
-// live variables
-var h = 10;
-var w = 10;
-var increment = 1;
-var limit = 1200;
+debug variables
+var h = 20;
+var w = 20;
+var increment = 8;
+var limit = 400;
 var timeInterval = 3000;
 var restartInterval = 300000;
 var final = 0;
+
+// live variables
+// var h = 10;
+// var w = 10;
+// var increment = 1;
+// var limit = 1200;
+// var timeInterval = 3000;
+// var restartInterval = 300000;
+// var final = 0;
 
 // *************************
 // ROUTERS
